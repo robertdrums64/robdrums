0960fec2d8d473d83209818b8635f7cef33f2afb
Robert De La Torre <r.delatorre@outlook.com>
2022-10-16 00:52:06 -0700

    new changes to homework 4

--- js/problem2a4.js
+++ js/problem2a4.js
@@ -1,5 +1,24 @@
 $(document).ready(function(){
-    $("button").click(function(){
-      $(".results").hide();
+
+  var rad1 = document.getElementById("radio1");
+  var rad2 = document.getElementById("radio2");
+
+    $("#radio1").click(function(){
+      $("#selected").text("Inches");
     });
-});
+    $("#radio2").click(function(){
+      $("#selected").text("Centimeters");
+    });
+
+    $("button").click(function(){
+
+      if (rad1.checked==true) {
+        alert("Inches was selected");
+      }
+      else if (rad2.checked==true) {
+        alert("Centimeters was selected");
+      }
+
+    });
+
+  });
