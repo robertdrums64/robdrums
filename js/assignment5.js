c5fee9553d482e1ff58ea7266a11ae15357ab17c
Robert De La Torre <r.delatorre@outlook.com>
2022-10-18 22:54:56 -0700

    added the car

--- js/assignment5.js
+++ js/assignment5.js
@@ -1,17 +1,40 @@
 $(document).ready(function() {
     var rand = Math.floor(Math.random() * 3) + 1;
 
+    // METHODS TO OPEN THE DOOR
+    // DOOR ONE
     $(".col1").click(function(){
         // alert(rand);
-        $("#door1").attr("src", "/images/doorOpen.png");
+        if (rand ==1) {
+            $("#door1").attr("src", "/images/winningDoor.png");
+            alert("You won!");
+        }
+        else {
+            $("#door1").attr("src", "/images/doorOpen.png"); 
+        }
     });
+    //DOOR TWO
     $(".col2").click(function(){
         // alert(rand);
-        $("#door2").attr("src", "/images/doorOpen.png");
+        if (rand == 2) {
+            $("#door2").attr("src", "/images/winningDoor.png");
+            alert("You won!");
+        }
+        else {
+            $("#door2").attr("src", "/images/doorOpen.png"); 
+        }
     });
+    //DOOR THREE
     $(".col3").click(function(){
         // alert(rand);
-        $("#door3").attr("src", "/images/doorOpen.png");
+        if (rand == 3) {
+            $("#door3").attr("src", "/images/winningDoor.png");
+            alert("You won!");
+        }
+        else {
+            $("#door3").attr("src", "/images/doorOpen.png"); 
+        }
     });
+
 
 });
