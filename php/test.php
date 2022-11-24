b0440081064a548936c2d010c4020cba60e23af7
Robert De La Torre <r.delatorre@outlook.com>
2022-11-24 01:11:35 -0800

    update

--- php/test.php
+++ php/test.php
@@ -9,7 +9,8 @@ $connect = new mysqli($servername, $username, $password, 3306);
 
 //Check my connection
 if($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    echo "NOT CONNECTED!"
+    // die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected Successfully!"
 
