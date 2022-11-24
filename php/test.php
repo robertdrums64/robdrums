73698663e79904e05856db95fac14717cdf6afac
Robert De La Torre <r.delatorre@outlook.com>
2022-11-24 01:07:57 -0800

    php tester added

---
+++ php/test.php
@@ -0,0 +1,16 @@
+<?php
+
+$servername = "localhost";
+$username = "rdelatorre";
+$password = "64Drummer!";
+
+//Create a connection
+$connect = new mysqli($servername, $username, $password, 3306);
+
+//Check my connection
+if($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected Successfully!"
+
+?>
